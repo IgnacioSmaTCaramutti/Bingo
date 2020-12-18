c50133a5592cbe5f38cb5ae8dc183200cb888d4e
Ignacio <ignaciocaramutti001@gmail.com>
2020-12-18 10:07:12 -0300

    Agrego un README

--- src/bingo.py
+++ src/bingo.py
@@ -1,2 +1,14 @@
-def saludo():
-    return "Hola."
+def carton():
+	carton = (
+                (1,0,1,1,1,0,1,0,1),
+		(0,1,0,1,0,1,1,1,1),
+		(0,1,0,0,1,0,0,1,0)
+	)
+	return carton
+
+def columna(carton, nmro_columna):
+	return(
+		carton[0][nmro_columna],
+		carton[1][nmro_columna],
+		carton[2][nmro_columna]
+                )
